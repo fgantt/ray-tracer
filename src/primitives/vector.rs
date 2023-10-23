37718a93344dcecd339cd5abe50796524906471e
Floyd Gantt <fgantt@gmail.com>
2023-10-23 05:30:55 -0500

    feat: vector magnitude, normalize, cross product and dot product

--- src/primitives/vector.rs
+++ src/primitives/vector.rs
@@ -8,6 +8,22 @@ pub struct Vector {
     z: f64
 }
 
+// ------------------------------------------------------
+impl Vector {
+    pub fn magnitude(&self) -> f64 {
+        f64::sqrt(self.x.powi(2) + self.y.powi(2) + self.z.powi(2))
+    }
+
+    pub fn normalize(&self) -> Vector {
+        *self / self.magnitude()
+    }
+
+    pub fn dot(&self, rhs: Vector) -> f64 {
+        self.x * rhs.x() + self.y * rhs.y() + self.z * rhs.z() + self.w() * rhs.w()
+    }
+}
+
+// ------------------------------------------------------
 impl Tuple for Vector {
     fn new(x: f64, y: f64, z: f64) -> Self {
         Vector { x, y, z }
@@ -74,6 +90,17 @@ impl std::ops::Mul<Vector> for f64 {
             rhs.y * self,
             rhs.z * self
         )
+    }
+}
+
+impl std::ops::Mul for Vector {
+    type Output = Vector;
+
+    fn mul(self, rhs: Vector) -> Self::Output {
+        Vector::new(
+            self.y * rhs.z() - self.z * rhs.y(),
+            self.z * rhs.x() - self.x * rhs.z(),
+            self.x * rhs.y() - self.y * rhs.x) 
     }
 }
 
@@ -156,9 +183,75 @@ mod tests {
 
     #[test]
     fn div_vector_by_scalae() {
-        let p = Vector::new(1.0, -2.0, 3.0);
-        let res = p / 2.0;
+        let v = Vector::new(1.0, -2.0, 3.0);
+        let res = v / 2.0;
         let expected = Vector::new(0.5, -1.0, 1.5);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn vector_magnitude() {
+        let v = Vector::new(1.0, 0.0, 0.0);
+        let res = v.magnitude();
+        assert_eq!(res, 1.0);
+
+        let v = Vector::new(0.0, 1.0, 0.0);
+        let res = v.magnitude();
+        assert_eq!(res, 1.0);
+
+        let v = Vector::new(0.0, 0.0, 1.0);
+        let res = v.magnitude();
+        assert_eq!(res, 1.0);
+
+        let v = Vector::new(1.0, 2.0, 3.0);
+        let res = v.magnitude();
+        assert_eq!(res, f64::sqrt(14.0));
+
+        let v = Vector::new(-1.0, -2.0, -3.0);
+        let res = v.magnitude();
+        assert_eq!(res, f64::sqrt(14.0));
+
+    }
+
+    #[test]
+    fn vector_normalize() {
+        // Scenario: Normalizing vector (4, 0, 0) gives (1, 0, 0)
+        let v = Vector::new(4.0, 0.0, 0.0);
+        let res = v.normalize();
+        let expected = Vector::new(1.0, 0.0, 0.0);
+        assert_eq!(res, expected);
+
+        // Scenario: Normalizing vector(1, 2, 3)
+        let v = Vector::new(1.0, 2.0, 3.0);
+        let res = v.normalize();
+        let magnitude = f64::sqrt(v.x().powi(2) + v.y().powi(2) + v.z().powi(2));
+        let expected = Vector::new(1.0 / magnitude, 2.0 / magnitude, 3.0 / magnitude);
+        assert_eq!(res, expected);
+
+        // Scenario: The magnitude of a normalized vector gives 1
+        let res = res.magnitude();
+        assert_eq!(res, 1.0);
+    }
+
+    #[test]
+    fn vector_dot_product() {
+        // Scenario: The dot product of two vectors
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        let v2 = Vector::new(2.0, 3.0, 4.0);
+        assert_eq!(v1.dot(v2), 20.0);
+    }
+
+    #[test]
+    fn vector_cross_product() {
+        // Scenario: The cross product of two vectors
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        let v2 = Vector::new(2.0, 3.0, 4.0);
+        let res = v1 * v2;
+        let expected = Vector::new(-1.0, 2.0, -1.0);
+        assert_eq!(res, expected);
+
+        let res = v2 * v1;
+        let expected = Vector::new(1.0, -2.0, 1.0);
         assert_eq!(res, expected);
     }
 }
